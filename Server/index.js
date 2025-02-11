02358d8486eadb9adecc8102b6595e36eca819c0
ramesh630392 <ramesh9912599@gmail.com>
2025-02-11 15:44:31 +0530

    updated to server less

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import {dbConnect} from "./config/db.js";
 import contactRouter from './routes/contactApi.js'
+import serverless from 'serverless-http';
 
 const app = express();
 app.use(express.json());
@@ -10,7 +11,9 @@ dotenv.config();
 
 app.use('/contacts', contactRouter);
 
-app.listen(3000, ()=>{
+/*app.listen(3000, ()=>{
     console.log("http://localhost:3000/contacts");
     dbConnect();
-});
+});*/
+
+export default serverless(app);
